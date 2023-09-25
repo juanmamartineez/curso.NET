9900a019f09cc0f86953ef471254bb662271d2b6
juamartinez <juamartinez@ext.sancorseguros.com>
2023-09-25 15:21:30 -0300

    Agrego controllador UserController

---
+++ curso.NET/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using curso.NET.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace curso.NET.Controllers
+{
+    [Route("Users")]
+    public class UserController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            User user = new User
+            {
+                Name = "Juan",
+                LastName = "Martinez",
+                Dni = 39323840,
+                Edad = 27
+            };
+
+            return View(user);
+        }
+    }
+}
---
+++ curso.NET/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace curso.NET.Models
+{
+    public class User
+    {
+        public string? Name { get; set; }
+
+        public string? LastName { get; set; }
+
+        public int? Dni { get; set; }
+
+        public int? Edad { get; set; }
+    }
+}
